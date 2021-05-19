b1bcb741ba31c489ad704b81aa964adc6ae2ff4e
hexh <hexh@yunjiglobal.com>
2021-05-19 10:12:45 +0800

    list-drop

---
+++ list-drop/src/App.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author: duiying
+ * @CreateDate: Do not edit
+ * @LastEditors: duiying
+ * @LastEditTime: 2021-05-18 17:06:56
+ * @Description: ...
+-->
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ list-drop/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: duiying
+ * @CreateDate: Do not edit
+ * @LastEditors: duiying
+ * @LastEditTime: 2021-05-18 17:06:44
+ * @Description: ...
+-->
+<template>
+  <div class="hello">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

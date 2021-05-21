6deedf8422c9af6e8f814ea24e6bc73b87c43cdd
hexh <hexh@yunjiglobal.com>
2021-05-21 18:14:14 +0800

    axios

--- list-drop/src/main.js
+++ list-drop/src/main.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: duiying
+ * @CreateDate: Do not edit
+ * @LastEditors: duiying
+ * @LastEditTime: 2021-05-21 17:56:57
+ * @Description: ...
+ */
 import Vue from 'vue'
 import App from './App.vue'
 
